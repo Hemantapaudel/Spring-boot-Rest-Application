13992d795c381102f582277ba074ff7ea8fc1457
Hemantapaudel <hemantapaudel05@gmail.com>
2017-05-24 02:47:27 +0530

    adding user entity apis

--- src/main/java/com/commom/application/data/dao/UserDAO.java
+++ src/main/java/com/commom/application/data/dao/UserDAO.java
@@ -1,24 +1,50 @@
 package com.commom.application.data.dao;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
+
 import com.commom.application.data.entity.User;
 
 @Component
 public class UserDAO {
-	
-    @Autowired
+
+	@Autowired
 	MongoOperations mongoTemplate;
 
 	public User saveUser(User user) {
-		 mongoTemplate.save(user);;
-		 return user;
+		mongoTemplate.save(user);
+		return user;
 	}
 
 	public List<User> getUsers() {
 		return mongoTemplate.findAll(User.class);
 	}
-	
+
+	public User findByUsername(String username) {
+		Query searchUserQuery = new Query(Criteria.where("username").is(
+				username));
+		User user = mongoTemplate.findOne(searchUserQuery, User.class);
+		System.out.println("2. find - savedUser : " + user);
+		return user;
+	}
+
+	public void updatePassword(String username) {
+		Query searchUserQuery = new Query(Criteria.where("username").is(
+				username));
+		mongoTemplate.updateFirst(searchUserQuery,
+				Update.update("password", "new password"), User.class);
+	}
+
+	public void deleteUser(String username) {
+		Query searchUserQuery = new Query(Criteria.where("username").is(
+				username));
+		mongoTemplate.remove(searchUserQuery, User.class);
+	}
+
 }
